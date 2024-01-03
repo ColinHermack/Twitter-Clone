f81fc4dee058aea2acf3941c25e15cd7c92ac0e9
Colin Hermack <chermack@purdue.edu>
2024-01-03 10:29:34 -0500

    Added animations to homepage

--- client/src/home.js
+++ client/src/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './home.css';
-import useLocation from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFeather, faHouse, faMagnifyingGlass, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.ScrollToTop = this.ScrollToTop.bind(this);
     }
 
     componentDidMount() {
@@ -13,9 +15,26 @@ class Home extends React.Component {
         console.log(currentPath);
     }
 
+    ScrollToTop() {
+        window.scrollTo(0, 0);
+    }
+
     render() {
-        return(<div id='container'>
-            <h1>Homepage</h1>
+        return(<div id='main-container'>
+            <nav>
+                <button id='navbar-logo'><FontAwesomeIcon icon={faFeather} /></button>
+                <button className='navbar-button'><FontAwesomeIcon icon={faHouse} /></button>
+                <button className='navbar-button'><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                <button className='navbar-button'><FontAwesomeIcon icon={faEnvelope} /></button>
+                <button className='navbar-button'><img src={undefined} alt="user's profile" className='navbar-item'/></button>
+                <div className='divider navbar-item'></div>
+            </nav>
+            <div id='top-menu'><FontAwesomeIcon icon={faFeather} id='top-menu-logo' /></div>
+            <div id='feed'></div>
+            <div id='news-panel'>
+                <h1>What's happening</h1>
+            </div>
+            <button id='post-button'><FontAwesomeIcon icon={faFeather} /></button>
         </div>)
     }
 }
